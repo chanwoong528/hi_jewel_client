07e36e00ae2ac6985173bf7aaa2090cfb4cd786e
moon <cksdnd004@naver.com>
2024-05-04 19:27:11 +0900

    loading flag success flag added

--- src/components/form/FormProductType.tsx
+++ src/components/form/FormProductType.tsx
@@ -1,6 +1,6 @@
 
 // import React from 'react'
-import { set, useForm } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
 import {
